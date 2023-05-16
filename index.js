6f0e041e2a5ceae020a9eb0255b300fd5f2971e8
NatáliaFonseca <fonsecanatalianf@gmail.com>
2023-05-16 17:15:36 -0300

    feat: integrate user initial validation

---
+++ index.js
@@ -0,0 +1,30 @@
+const validateUser = async (email) =>{
+try {
+const result = await fetch (`https://mp-wallet-app-api.herokuapp.com/users?email=${email}`);
+
+const user=await result.json();
+return user;
+} catch (error){
+return {error};
+
+}
+};
+
+const onClickLogin =async()=>{
+  const email=document.getElementById('input-email').value;
+  if(email.length < 5 || !email.includes ('@')){
+    alert("Email Inválido!")
+    return;
+  }
+const result=await validateUser(email);
+
+if (result.error){
+  alert ("falha ao validar o e-mail");
+  return;
+}
+
+localStorage.setItem("@WalletApp:userEmail", result.email);
+localStorage.setItem("@WalletApp:userName", result.name);
+localStorage.setItem("@WalletApp:userId", result.id);
+window.open("./src/pages/home/index.html", "_self");
+};
